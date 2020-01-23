c0535f98f836b3e79f6e2469e3ef15217f7c3231
Ritvik Upadhyaya <rupadhyaya@apple.com>
2020-01-22 23:02:07 -0800

    css->scss, added some links and moved things around.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './index.scss';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Navbar from 'react-bootstrap/Navbar';
 import CreativeTitle from './components/CreativeTitle';
-import FadeInSubtitle from './components/FadeInSubtitle';
+
 
 class App extends Component {
   render() {
-    const subtitleFadeDuration = 700;
-    const subtitleFadeInitialDelay = 200;
-    const subtitlesToFadeIn = ["Engineer. ", "Photographer. ", "Nomad."];
-    
-    const subtitlesToRender = subtitlesToFadeIn.map((subtitle, index) =>
-      <FadeInSubtitle title={subtitle}
-                      animDelay={subtitleFadeInitialDelay + index*subtitleFadeDuration}
-                      animDuration={subtitleFadeDuration}/>
-    );
-
     return (
-      <div className="App">
-        <div className="App-header">
-          {/*
-            <img src={logo} className="App-logo" alt="logo" />
-          */}
-          <CreativeTitle className="App-title" title="Ritvik Upadhyaya"/>
-          <div className="App-subtitle">
-            {subtitlesToRender}
-          </div>
-          {/*
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          */}
-        </div>
+      <Router>
+      <div>
+      <Navbar>
+        <Navbar.Brand href="/">
+          <div ><CreativeTitle title="Ritvik Upadhyaya"/></div>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Text>
+          <a className="navlink" href="/about">About</a>
+        </Navbar.Text>
+      </Navbar>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import '../App.scss';
+import CreativeTitle from '../components/CreativeTitle';
+import FadeInSubtitle from '../components/FadeInSubtitle';
+
+class Home extends Component {
+  render() {
+    const subtitleFadeDuration = 700;
+    const subtitleFadeInitialDelay = 200;
+    const subtitlesToFadeIn = ["Engineer. ", "Photographer. ", "Nomad."];
+
+    const subtitlesToRender = subtitlesToFadeIn.map((subtitle, index) =>
+      <FadeInSubtitle title={subtitle}
+                      animDelay={subtitleFadeInitialDelay + index*subtitleFadeDuration}
+                      animDuration={subtitleFadeDuration}/>
+    );
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <CreativeTitle className="App-title" title="Ritvik Upadhyaya"/>
+          <div className="App-subtitle">
+            {subtitlesToRender}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
