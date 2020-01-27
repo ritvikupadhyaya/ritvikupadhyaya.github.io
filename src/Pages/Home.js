826a591f58bbe23b8e90e362a0c2698c3f4837e6
Ritvik Upadhyaya <rupadhyaya@apple.com>
2020-01-26 21:19:01 -0800

    Particle js working finally

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -17,15 +17,66 @@ class Home extends Component {
                       animDuration={subtitleFadeDuration}/>
     );
 
+    const particlesOptions = {
+	    "particles": {
+	        "number": {
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+	};
+
     return (
         <div className="Page-Content">
+        <div className="Particles-background">
+          <Particles params={particlesOptions}/>
+        </div>
         <div style={{textAlign: "center"}}>
           <CreativeTitle className="App-title" title="Ritvik Upadhyaya"/>
           <div className="App-subtitle">
             {subtitlesToRender}
           </div>
         </div>
-      </div>
+        </div>
     );
   }
 }
